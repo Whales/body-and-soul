78f31561085c7d1cb2253d7cc6932b9837b4ddc8
Whales <fivedozewhales@gmail.com>
2012-11-01 00:43:29 -0400

    Data saving function.

--- editor.cpp
+++ editor.cpp
@@ -16,6 +16,10 @@ int main()
  w_main.outline();
  w_main.putstr( 1, 0, c_black, c_white, "<c=,ltblue>S<c=/>tats");
  w_main.line_v(26);
+ int line = 1;
+ for (std::list<body_stat>::iterator it = BODY_STATS_POOL.begin();
+      it != BODY_STATS_POOL.end() && line < 23; it++)
+  w_main.putstr(1, line, c_white, c_black, "%s - %d", it->name, it->value);
  w_main.putstr(27, 0, c_black, c_white, "<c=,ltblue>P<c=/>arts");
  w_main.line_v(52);
  w_main.putstr(53, 0, c_black, c_white, "<c=,ltblue>B<c=/>odies");
