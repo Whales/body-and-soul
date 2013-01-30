9bb002626d64639bc7ee40083a10bc24a23bd219
Whales <fivedozewhales@gmail.com>
2013-01-30 02:31:42 -0500

    Some minor mapgen loading work

--- mapgen.cpp
+++ mapgen.cpp
@@ -1,3 +1,15 @@
 #include "mapgen.h"
 
+virtual void mapgen_spec::load_data(std::istream &datastream)
+{
+  debugmsg("ran load_data on flavorless mapgen_spec");
+  return;
+}
 
+virtual void mapgen_spec_builings::load_data(std::istream &datastream)
+{
+  std::string id;
+  do {
+    datastream >> id;
+    id = no_caps(id);
+  } while (id != "done");
--- mapgen.h
+++ mapgen.h
@@ -58,5 +58,11 @@ struct mapgen_spec_heightmap : public mapgen_spec
 
   std::vector<heightmap> maps;
 };
+
+struct mapgen_spec_container()
+{
+  void load_data(std::istream &datastream);
+  std::vector<mapgen_spec*> specs;
+};
   
 #endif
